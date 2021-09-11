6c39fe9baf30beeb10646c99e9b269d6b0885d1f
Steve Desmond <steve@vtsv.ca>
2021-09-11 15:47:53 -0400

    App WIP

---
+++ app/MotoG/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MotoG.Models;
+using MotoG.Services;
+
+namespace MotoG.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly IPhoneInfoService _phoneInfo;
+		private readonly IPriceDisplayService _priceDisplay;
+
+		public HomeController(IPhoneInfoService phoneInfo, IPriceDisplayService priceDisplay)
+		{
+			_phoneInfo = phoneInfo;
+			_priceDisplay = priceDisplay;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var phones = await _phoneInfo.GetAll();
+			return View(phones);
+		}
+
+		public async Task<IActionResult> Info(string id)
+		{
+			var phoneInfo = await _phoneInfo.GetInfo(id);
+			var displayInfo = new List<PriceDisplay>();
+			foreach (var p in phoneInfo)
+			{
+				var pd = new PriceDisplay
+				{
+					PhoneInfo = p,
+					LogoURL = await _priceDisplay.GetLogoURL(p),
+					Price = await _priceDisplay.GetPrice(p)
+				};
+				displayInfo.Add(pd);
+			}
+
+			foreach (var info in displayInfo)
+			{
+				var average = displayInfo.Average(i => i.Price);
+				info.PercentOfAverage = info.Price / average;
+			}
+
+			return View(displayInfo);
+		}
+	}
+}
---
+++ app/MotoG/Services/PhoneInfoService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using MotoG.Models;
+
+namespace MotoG.Services
+{
+	public class PhoneInfoService : IPhoneInfoService
+	{
+		private readonly IDbConnection _db;
+
+		public PhoneInfoService(IDbConnection db)
+			=> _db = db;
+
+		public async Task<IEnumerable<PhoneInfo>> GetAll()
+			=> await _db.QueryAsync<PhoneInfo>("SELECT * FROM PhoneInfo");
+
+		public async Task<IEnumerable<PhoneInfo>> GetInfo(string name)
+		{
+			var phones = await GetAll();
+			return phones.Where(p => p.Name == name);
+		}
+	}
+}
