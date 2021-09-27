dcb962b1d6eeb7422c3775d366cd72ee25c48717
Steve Desmond <steve@vtsv.ca>
2021-09-27 10:43:12 -0400

    Cache database query results

--- app/Phones/Services/PhoneInfoService.cs
+++ app/Phones/Services/PhoneInfoService.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Threading.Tasks;
 using Dapper;
+using Microsoft.Extensions.Caching.Memory;
 using Phones.Models;
 
 namespace Phones.Services
@@ -9,17 +11,36 @@ namespace Phones.Services
 	public class PhoneInfoService : IPhoneInfoService
 	{
 		private readonly IDbConnection _db;
+		private readonly IMemoryCache _cache;
 
-		public PhoneInfoService(IDbConnection db)
-			=> _db = db;
+		public PhoneInfoService(IDbConnection db, IMemoryCache cache)
+		{
+			_db = db;
+			_cache = cache;
+		}
 
 		public async Task<IEnumerable<PhoneInfo>> GetAll()
-			=> await _db.QueryAsync<PhoneInfo>("SELECT * FROM PhoneInfo");
+			=> await _cache.GetOrCreateAsync("All",
+				async entry =>
+				{
+					entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1);
+					return await _db.QueryAsync<PhoneInfo>("SELECT * FROM PhoneInfo");
+				});
 
 		public async Task<IEnumerable<PhoneInfo>> GetInfo(string name)
-			=> await _db.QueryAsync<PhoneInfo>("SELECT * FROM PhoneInfo WHERE Name = @Name", new { Name = name });
+			=> await _cache.GetOrCreateAsync(name,
+				async entry =>
+				{
+					entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1);
+					return await _db.QueryAsync<PhoneInfo>("SELECT * FROM PhoneInfo WHERE Name = @Name", new { Name = name });
+				});
 
 		public async Task<IEnumerable<string>> GetNames()
-			=> await _db.QueryAsync<string>("SELECT DISTINCT Name, LENGTH(Name) FROM PhoneInfo ORDER BY LENGTH(Name), Name");
+			=> await _cache.GetOrCreateAsync("Names",
+				async entry =>
+				{
+					entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1);
+					return await _db.QueryAsync<string>("SELECT DISTINCT Name, LENGTH(Name) FROM PhoneInfo ORDER BY LENGTH(Name), Name");
+				});
 	}
 }
