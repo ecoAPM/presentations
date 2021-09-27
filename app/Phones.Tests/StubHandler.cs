1665ea5a3056621d85acd7aec3464453674139e8
Steve Desmond <steve@vtsv.ca>
2021-09-27 09:11:15 -0400

    Migrate from AngleSharp to HttpClient

---
+++ app/Phones.Tests/StubHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Phones.Tests
+{
+	internal class StubHandler : HttpMessageHandler
+	{
+		private readonly string _response;
+
+		public StubHandler(string response) => _response = response;
+
+		protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			var msg = new HttpResponseMessage(HttpStatusCode.OK)
+			{
+				Content = new StringContent(_response)
+			};
+			return Task.FromResult(msg);
+		}
+	}
+}
